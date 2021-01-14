56b0ee75463a947d3877a6848e3f510a5050e160
khadyCi <khady.cis@educa.jcyl.es>
2021-01-14 20:22:44 +0100

    commit 2

--- Modelo/webconfig.php
+++ Modelo/webconfig.php
@@ -1,7 +1,7 @@
 <?php
 define('DNS','mysql:dbname=api_rest_frameworks;host=localhost');  
-define('USERNAME','root');  
-define('PASSWORD','toor');
+define('USERNAME','khady');  
+define('PASSWORD','khady');
 ?>
 
 		
